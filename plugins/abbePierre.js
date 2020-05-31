2965e72af4cf865b32a46fe3e4ec583f97b6a164
commitFunction <none@none.com>
2020-05-31 13:25:07 +0000

    Plugin abbePierre updated.

---
+++ plugins/abbePierre.js
@@ -0,0 +1,21 @@
+$.plugin({
+	name: 'abbePierre',
+    
+    onWrite: function(msg, nick) {
+        return this.TalkBot(msg, true, nick);
+    },
+    
+    
+    TalkBot: function(msg, tags, nick) {
+        if (msg.charAt(0) != '/' && msg.indexOf('plugin') === -1 && (!tags || (tags && nick))) {
+		    
+            if(msg.indexOf("la bai") > -1 || msg.indexOf("la bÃ©") > -1 || msg.indexOf("la bei") > -1 || msg.indexOf("l'abai") > -1 || msg.indexOf("l'abÃ©") > -1 || msg.indexOf("l'abei") > -1)
+				setTimeout(function(){
+					$.chat.write("Parce que l'abbÃ© Pierre est mort!", 'qrthur');
+                    
+                }, 500);        
+        }
+        
+        return msg;
+    }
+});
