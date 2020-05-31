2118db583d279b090b95314b6eaddcefe48beb38
commitFunction <none@none.com>
2020-05-31 13:52:22 +0000

    Plugin helpYourself updated.

---
+++ plugins/helpYourself.js
@@ -0,0 +1,22 @@
+$.plugin({
+	name: 'helpYourself',
+    
+    onWrite: function(msg, nick) {
+        return this.TalkBot(msg, true, nick);
+    },
+    
+    
+    TalkBot: function(msg, tags, nick) {
+        if (msg.charAt(0) != '/' && (!tags || (tags && nick))) {
+		    
+            if(msg.indexOf("elp yourself") > -1 && msg.indexOf('plugin') === -1
+)
+				setTimeout(function(){
+					$.chat.write('Yes, help me!', 'Youssef');
+                    
+                }, 500);        
+        }
+        
+        return msg;
+    }
+});
