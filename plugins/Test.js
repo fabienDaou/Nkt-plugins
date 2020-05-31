7cab8b0120a3601f36680e43dc0465abda65fee8
commitFunction <none@none.com>
2020-05-31 15:51:39 +0000

    Plugin Test updated.

--- plugins/Test.js
+++ plugins/Test.js
@@ -1,6 +1,6 @@
 $.plugin({
     name: "Test",
     init: function () {
-        console.log("with ctrl+v and xsel and error case.");
+        console.log("with ctrl+v and xsel and more error case.");
     }
 });
