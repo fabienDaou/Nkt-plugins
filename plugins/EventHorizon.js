63502f9da895fa05a8a0af67059bd8ba7aac6e58
commitFunction <none@none.com>
2020-05-31 13:22:04 +0000

    Plugin EventHorizon updated.

---
+++ plugins/EventHorizon.js
@@ -0,0 +1,39 @@
+var eventHorizon = {
+    lineId: "eventHorizon",
+    onBlur: function () {
+        const chat = document.getElementById("chat");
+        if (chat.childNodes) {
+            const lineNode = document.createElement("hr");
+            lineNode.setAttribute("id", this.lineId);
+            lineNode.style.display = "none";
+            chat.prepend(lineNode);
+        }
+    },
+    onFocus: function () {
+        const lineNode = document.getElementById(this.lineId);
+        if (lineNode.parentNode.firstChild != lineNode) {
+            lineNode.style.display = "block";
+            lineNode.scrollIntoView({ behavior: "smooth", block: "center" });
+            lineNode.style.opacity = "0";
+            lineNode.style.transition = "visibility 0s 2s, opacity 2s linear";
+            setTimeout(() => {
+                lineNode.remove();
+            }, 2000);
+        }
+        else {
+            lineNode.remove();
+        }
+    }
+};
+
+$.plugin({
+    name: 'EventHorizon',
+    init: function () {
+        window.addEventListener("blur", eventHorizon.onBlur);
+        window.addEventListener("focus", eventHorizon.onFocus);
+    },
+    stop: function () {
+        window.removeEventListener("blur", eventHorizon.onBlur);
+        window.removeEventListener("focus", eventHorizon.onFocus);
+    }
+});
