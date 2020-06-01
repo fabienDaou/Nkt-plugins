694040cf49c3537de89991a96eeffbd3ab257bbf
commitFunction <none@none.com>
2020-06-01 18:04:37 +0000

    Plugin seed updated.

---
+++ plugins/seed.js
@@ -0,0 +1,80 @@
+$.plugin({
+    name: 'seed',
+    init: function () {
+        var loadAndLog = function (pluginName) {
+            console.log(pluginName + " is loading...");
+            $.pluginApi.loadPlugin(pluginName);
+        };
+        $.pluginApi.unloadPlugin('TalkBot');
+        loadAndLog('time');
+        loadAndLog('EyePopping');
+        loadAndLog('IRCcmd');
+        loadAndLog('removeBeta');
+        loadAndLog('EventHorizon');
+        loadAndLog('NewMsgTitle');
+        loadAndLog('PluginV2');
+
+        var plugins = [
+            'summon',
+            'userprefs',
+            'plus1djbz',
+            'KrakoBot',
+            'Djambot',
+            'daouspec',
+            'randomFacts',
+            'Jahan',
+            'mention',
+            'deutsch',
+            'KaamelottBot',
+            'coolnick',
+            'helpYourself',
+            'KevinBot',
+            'BaitKrako',
+            'rulesOfTheInternet',
+            'bitschyPlz',
+            'canadiens',
+            'Bobbot',
+            'zussa',
+            'cara',
+            'abbePierre',
+            'roll',
+            'benspec',
+            'meme',
+            'qrBot',
+            'DeniseBot',
+            'gifs',
+            'Markov',
+            'Anecbot',
+            'pwgen',
+            'darryckBot',
+            'zafBot',
+            'todo',
+            'quote',
+            'selfBot',
+            'nktrpg',
+            'barBot',
+            'neyBot',
+            'noF5',
+            'blindtest'];
+
+        var loadPluginsWhenNotLooking = function (plugins) {
+            if (plugins.length !== 0) {
+                var pluginToLoad = plugins.shift();
+                loadAndLog(pluginToLoad);
+                setTimeout(() => {
+                    if (!looking) {
+                        loadPluginsWhenNotLooking(plugins);
+                    }
+                }, 10);
+            }
+        };
+        var looking;
+        $(window).focus(function () {
+            looking = true;
+        });
+        $(window).blur(function () {
+            looking = false;
+            loadPluginsWhenNotLooking(plugins);
+        });
+    }
+});
