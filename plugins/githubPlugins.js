72b6f233ddb1960dc2ae198273d8e3943d8ce44e
fabienDaou <fabien.daoulas@gmail.com>
2020-06-07 17:27:08 +0200

    Plugin githubPlugins created.

---
+++ plugins/githubPlugins.js
@@ -0,0 +1,45 @@
+$.plugin({
+    name: 'githubPlugins',
+    init: function() {
+      window.removeEventListener("message", $.chat.githubPluginsListener);
+      $.chat.githubPluginsListener = this.receiveMessage;
+      window.addEventListener("message", $.chat.githubPluginsListener, false);
+      if (!document.getElementById('githubPlugins')) {
+        $('#plugin-container').prepend('<iframe id="githubPlugins" style="border:0;height:50px;width:200px" src="https://fabiendaou.github.io/index.html"></iframe>');
+      }
+    },
+    stop: function() {
+      window.removeEventListener("message", $.chat.githubPluginsListener);
+      $.chat.githubPluginsListener = null;
+      $('#githubPlugins').remove();
+    },
+    receiveMessage(event) {
+      if (event.origin !== "https://fabiendaou.github.io")
+        return;
+      try {
+        const eventData = JSON.parse(event.data);
+        console.log(eventData);
+        switch (eventData.event) {
+          case 'pluginsLoaded':
+            for (let plugin of eventData.data) {
+                //console.log(plugin);
+                try {
+                    try {
+                        // JSON ?
+                        let json = JSON.parse(plugin.text);
+                        plugin = atob(json.content);
+                    } catch(e) { // Raw
+                    } finally {
+                        eval(plugin.text);
+                    }
+                } catch (e) { console.error(e); }
+                
+            }
+            break;
+          default:
+            console.log('Cannot understand message from iframe');
+            console.log(eventData);
+        }
+      } catch(e) {console.error(e);}
+    }
+});
