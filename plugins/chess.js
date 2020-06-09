356b2fa5b2df2971df4529b7a0eeaaf7f506987c
maxime gillet <gilet666@hotmail.fr>
2020-06-09 19:21:09 +0200

    Update chess.js
    ok chess fix

--- plugins/chess.js
+++ plugins/chess.js
@@ -236,10 +236,9 @@ var chessPlugin = function () {
 	
 		//check if a piece was taken
 		var match = regex.exec(content);
-		
-		console.log("[chess] score debug match :"+ content);
+	
 		//update player score for taken piece
-		if(match[1]){
+		if(match && match[1]){
 			scores[player] += pieceValue[match[1]];
 			displayScore();
 		}	
