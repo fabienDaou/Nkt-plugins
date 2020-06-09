e9411a6a4f07be0dad18a764d95aeb715f0747f6
a <a@a.a>
2020-06-09 10:48:35 +0200

    fix view link

--- plugins/removeBeta.js
+++ plugins/removeBeta.js
@@ -11,6 +11,13 @@ $.plugin({
             }).each(function() {
               var elem = $(this);
               elem.remove();
+              $(elem).find('a').attr(
+                  'href',
+                  (($.chat.privatePlugins
+                  && $.chat.privatePlugins.indexOf($(this).attr('id')) > -1)
+                  ? 'https://github.com/fabienDaou/Nkt-plugins-private/blob/master/plugins/'+ $(this).attr('id') +'.js'
+                  : 'https://github.com/fabienDaou/Nkt-plugins/blob/master/plugins/'+ $(this).attr('id') +'.js')
+                );
               $(elem).appendTo("#plugin-container");
             });
              $.chat.chatObserver.observe(document.getElementById('plugin-container'), {
@@ -20,7 +27,8 @@ $.plugin({
         $.chat.chatObserver = new MutationObserver(function (mutations) {
             mutations.forEach(({ addedNodes }) => {
                 if (addedNodes !== null) {
-                    clearTimeout($.chat.timerdaousort);$.chat.timerdaousort=setTimeout($.chat.fndaousort, 100);
+                    clearTimeout($.chat.timerdaousort);
+                    $.chat.timerdaousort=setTimeout($.chat.fndaousort, 100);
                 }
             });
         });
