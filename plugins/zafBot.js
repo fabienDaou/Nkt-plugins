40f156f7e991e6658729135fa8ece147f14a916e
qr7hur <qr7hur@gmail.com>
2020-12-28 22:37:39 +0100

    Update zafBot.js

--- plugins/zafBot.js
+++ plugins/zafBot.js
@@ -5,7 +5,7 @@ $.plugin({
   },
   zafBot: function(msg, tags, nick) {
     if (msg.charAt(0) != '/' && msg.indexOf('plugin') === -1  && (!tags || (tags && nick))) {
-      var ZafArray = ["Je suis un lézard", "t'es mauvais", "je m'ennuie", "c'est nul", "je bouffe pas les dynos moi"];
+      var ZafArray = ["Je suis un lézard", "t'es mauvais", "je m'ennuie", "c'est nul", "je bouffe pas les dynos moi", "le nasus m'a buvette"];
       if(msg.indexOf("zaf") > -1 && nick == $.chat.myNick() && Math.random() > 0.65) {
         var rnd = Math.floor(Math.random()*ZafArray.length);
         $.chat.send('°Zafquote '+rnd.toString());
