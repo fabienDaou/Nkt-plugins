da81cf3f2bf12c44a2b28ef034d53adf16108c3e
qr7hur <qr7hur@gmail.com>
2022-12-03 20:48:45 +0100

    Create muteTheLeecher.js

---
+++ plugins/muteTheLeecher.js
@@ -0,0 +1,12 @@
+$.plugin({
+    
+    name: 'yo',
+    onNewUser: function(nick) {
+        if (nick != $.chat.myNick() && nick.indexOf('leech_') === 0 ) {
+            if (!$.chat.isMuted(nick)) $.chat.mute(nick);
+        }
+    },
+    init: function() {
+        if (!$.chat.isMuted(nick)) $.chat.mute(nick);
+    }
+});
