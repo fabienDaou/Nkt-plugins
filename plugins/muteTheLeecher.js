3439d1164723f8a5107a5fdcf48a0b9ae91432b5
qr7hur <qr7hur@gmail.com>
2022-12-04 10:07:16 +0100

    Update muteTheLeecher.js

--- plugins/muteTheLeecher.js
+++ plugins/muteTheLeecher.js
@@ -10,5 +10,6 @@ $.plugin({
         for (let nick of $.chat.nicks()) {
             if (nick && nick.indexOf('leech_') === 0 && !$.chat.isMuted(nick)) setTimeout(function(){$.chat.mute(nick)}, 10);
         }
+        if ($.chat.myNick().indexOf('leech_') === 0) setTimeout(function(){window.location.reload()}, 86400000);
     }
 });
