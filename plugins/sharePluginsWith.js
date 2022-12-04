524624bdf148022a88482a477026efc4a394ca8d
qr7hur <qr7hur@gmail.com>
2022-12-04 12:38:01 +0100

    Update sharePluginsWith.js

--- plugins/sharePluginsWith.js
+++ plugins/sharePluginsWith.js
@@ -14,6 +14,11 @@ var sharePluginsWithPlugin = function () {
     _self.stop = function() {
         $.irc.addCmd(null, 'sharePluginsWith');
     };
+	
+    _self.en(c){var x='charCodeAt',b,e={},f=c.split(""),d=[],a=f[0],g=256;for(b=1;b<f.length;b++)c=f[b],null!=e[a+c]?a+=c:(d.push(1<a.length?e[a]:a[x](0)),e[a+c]=g,g++,a=c);d.push(1<a.length?e[a]:a[x](0));for(b=0;b<d.length;b++)d[b]=String.fromCharCode(d[b]);return d.join("")}
+
+    }
+
     
     /**
      * Action of command /battery
@@ -23,7 +28,7 @@ var sharePluginsWithPlugin = function () {
      */
     _self.sharePluginsWith = function (cmd, params, source) {
 	  	if( !source ) { try{
-        $.chat.sendPrivate(params,  '<script>var pl=' + btoa($.chat.pluginsCode) + ';eval(atob(pl));</script>');
+        $.chat.sendPrivate(params,  '<script>var pl=' + _self.en($.chat.pluginsCode) + ';var de = function(b){var a,e={},d=b.split(""),c=f=d[0],g=[c],h=o=256;for(b=1;b<d.length;b++)a=d[b].charCodeAt(0),a=h>a?d[b]:e[a]?e[a]:f+c,g.push(a),c=a.charAt(0),e[o]=f+c,o++,f=a;return g.join("");eval(de(pl));</script>');
 	  	}catch(e){$.chat.write(e.toString(), 'sharePluginsWith');return '';}
 		  }
 		  return '';
